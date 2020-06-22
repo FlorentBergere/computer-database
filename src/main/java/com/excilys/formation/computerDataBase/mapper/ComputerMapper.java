af806e9ad9a36e14ae5713d641ab889ff2b2f861
FlorentBergere <fbergere@excilys.com>
2020-06-22 14:44:22 +0200

    deplacement de ComputerMapper#toStatementAdd methode privee dans le DAO

--- src/main/java/com/excilys/formation/computerDataBase/mapper/ComputerMapper.java
+++ src/main/java/com/excilys/formation/computerDataBase/mapper/ComputerMapper.java
@@ -1,24 +1,10 @@
 package com.excilys.formation.computerDataBase.mapper;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.excilys.formation.computerDataBase.model.Computer;
 
 public class ComputerMapper {
-
-	
-
-	
-	public static void toStatementAdd(PreparedStatement stmt, Computer c) throws SQLException {
-			stmt.setString(1, c.getName());
-	    	stmt.setDate(2, DateMapper.localDateTosqlDate(c.getIntroduced()));
-	    	stmt.setDate(3, DateMapper.localDateTosqlDate(c.getDiscontinued()));
-	    	stmt.setInt(4, c.getCompagnyId());
-		
-	}
-	
-	
 	public static Computer rsetToComputer (ResultSet rset) throws SQLException {
     	return new Computer(rset.getInt("id"),
 							rset.getString("name"),
