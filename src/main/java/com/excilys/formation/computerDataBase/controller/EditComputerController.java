7269f43ec2534354a931a27f3dd49e8146b55522
FlorentBergere <fbergere@excilys.com>
2020-07-15 17:42:55 +0200

    editComputer ok

--- src/main/java/com/excilys/formation/computerDataBase/controller/EditComputerController.java
+++ src/main/java/com/excilys/formation/computerDataBase/controller/EditComputerController.java
@@ -1,20 +1,13 @@
 package com.excilys.formation.computerDataBase.controller;
 
-import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.excilys.formation.computerDataBase.model.DTO.CompanyDTO;
 import com.excilys.formation.computerDataBase.model.DTO.ComputerDTO;
 import com.excilys.formation.computerDataBase.service.EditComputerService;
@@ -31,65 +24,42 @@ public class EditComputerController  {
        
 	
     @GetMapping("editComputer")
-	public void editComputer() {
+	public String geteditComputer(
+			@RequestParam("computerId") String computerId,
+			@RequestParam("computerName") String name,
+			@RequestParam("introduced") String introduced,
+			@RequestParam("discontinued") String discontinued,
+			@RequestParam("companyId") String companyId,
+			ModelMap map) {
+    	List<CompanyDTO>companyDTOcollection = editComputerService.getListCompany();
+    	map.put("listCompany", companyDTOcollection);
+    	
+    	map.put("computerId", computerId);
+    	map.put("computerName", name);
+    	map.put("introduced", introduced);
+    	map.put("discontinued", discontinued);
+    	map.put("companyId", companyId);
+    	
+    
+    	return "editComputer";
+    }
+    
+    
+    @PostMapping("editComputer")
+   	public String editComputer (
+			@RequestParam("computerId") String computerId,
+			@RequestParam("computerName") String name,
+			@RequestParam("introduced") String introduced,
+			@RequestParam("discontinued") String discontinued,
+			@RequestParam("companyId") String companyId,
+			ModelMap map) {
+    	
+    	CompanyDTO companyDTO = editComputerService.findCompanyById(companyId);	
+		ComputerDTO computerDTO = new ComputerDTO(computerId, name, introduced, discontinued, companyDTO);
+    	editComputerService.editComputer(computerDTO);
+    	
+    	return "editComputer";
     	
     }
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public EditComputerController() {
-//        super();
-//    }
-//    
-//
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		
-//		String computerId = request.getParameter("computerId");
-//		String computerName = request.getParameter("computerName");
-//		String introduced = request.getParameter("introduced");
-//		String discontinued = request.getParameter("discontinued");
-//		String companyId = request.getParameter("companyId");
-//		
-//
-//		request.setAttribute("computerId",computerId);
-//		request.setAttribute("computerName",computerName);
-//		request.setAttribute("introduced",introduced);
-//		request.setAttribute("discontinued",discontinued);
-//		request.setAttribute("companyId",companyId);
-//		
-//		List<CompanyDTO> companyDTOcollection = editComputerService.getListCompany();
-//		request.setAttribute("listCompany", companyDTOcollection);
-//				
-//		System.out.println(computerId+"#"+computerName+"#"+introduced+"#"+discontinued+"#"+companyId);
-//		
-//		request.getRequestDispatcher("views/editComputer.jsp").forward(request,response);
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		
-//		String id = request.getParameter("computerId");
-//		String name = request.getParameter("computerName");
-//		String introduced = request.getParameter("introduced");
-//		String discontinued = request.getParameter("discontinued");
-//		String compagnyId = request.getParameter("companyId");
-//		
-//		
-//		CompanyDTO companyDTO = editComputerService.findCompanyById(compagnyId);	
-//		ComputerDTO computerDTO = new ComputerDTO(id, name, introduced, discontinued, companyDTO);
-//		
-//		editComputerService.editComputer(computerDTO);
-//		
-//		doGet(request, response);
-//	}
 
 }
