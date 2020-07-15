dff9be1b4e9d5f184f55a602c066a61809d45443
FlorentBergere <fbergere@excilys.com>
2020-07-15 14:49:07 +0200

    controller list dto

--- src/main/java/com/excilys/formation/computerDataBase/controller/DashBoardController.java
+++ src/main/java/com/excilys/formation/computerDataBase/controller/DashBoardController.java
@@ -1,28 +1,16 @@
 package com.excilys.formation.computerDataBase.controller;
 
-import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-
-import com.excilys.formation.computerDataBase.mapper.ComputerMapper;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.excilys.formation.computerDataBase.model.Computer;
 import com.excilys.formation.computerDataBase.model.DTO.ComputerDTO;
 import com.excilys.formation.computerDataBase.service.DashBoardService;
 
-/**
- * Servlet implementation class DashBoard
- */
 
 @Controller
 public class DashBoardController {
@@ -30,42 +18,34 @@ public class DashBoardController {
 	@Autowired
 	private DashBoardService dashBoardService;
 	
-	private int pageNumber = 0;
-	private int nbEntryPerPage = 10;
 	private int numberMaxPage;
 	private List<Integer> listPage;
-    private String search;
     private List<ComputerDTO> computerDTOCollection;
-    private Computer.atributes atribute;
        
 
     @GetMapping("dashboard")
-	public void dashboard() {
+	public void dashboard(
+			@RequestParam(value = "pageNumber ", defaultValue = "0") int pageNumber,
+			@RequestParam(value = "nbEntryPerPage ", defaultValue = "10") int nbEntryPerPage,
+			@RequestParam(value = "search", required = false) String search,
+    		@RequestParam(value = "atribute", required = false) Computer.atributes atribute,
+    		ModelMap map){
+    	
+    	computerDTOCollection = dashBoardService.findAllByPage(nbEntryPerPage,pageNumber);
+    	numberMaxPage = dashBoardService.getNumberMaxPage();
+    	listPage = dashBoardService.getListPage();
+    	map.put("computerDTOCollection", computerDTOCollection);
+    	map.put("nbComputer", dashBoardService.countComputer());
+    	map.put("nbPage", numberMaxPage);
+    	map.put("listPage", listPage);
+    	map.put("pageNumber", pageNumber);
+    	map.put("nbEntryPerPage", nbEntryPerPage);
     	
     }
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public DashBoardController() {
-//        super();
-//    }
-//
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		
-//		if (request.getParameter("pageNumber") != null) {
-//			pageNumber = Integer.valueOf(request.getParameter("pageNumber"));
-//		}
-//		
-//		if (request.getParameter("nbEntryPerPage") != null) {
-//			nbEntryPerPage = Integer.valueOf(request.getParameter("nbEntryPerPage"));
-//		}
-//		
+
+
+	
+	
 //		atribute = ComputerMapper.parseAtribute((request.getParameter("orderBy")));
 //		
 //		search = request.getParameter("search");
@@ -82,19 +62,7 @@ public class DashBoardController {
 //				computerDTOCollection = dashBoardService.findAllByPage(nbEntryPerPage,pageNumber,atribute);
 //			}
 //		}
-//		
-//		
-//
-//		numberMaxPage = dashBoardService.getNumberMaxPage();
-//		listPage = dashBoardService.getListPage();
-//		request.setAttribute("computerDTOCollection", computerDTOCollection);
-//		request.setAttribute("nbComputer", dashBoardService.countComputer());
-//		request.setAttribute("nbPage", numberMaxPage);
-//		request.setAttribute("listPage", listPage);
-//		request.setAttribute("pageNumber", pageNumber);
-//		request.setAttribute("nbEntryPerPage", nbEntryPerPage);
-//		
-//
+
 //		
 //		request.getRequestDispatcher("views/dashboard.jsp").forward(request,response);
 //	}
